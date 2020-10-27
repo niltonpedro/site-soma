a361aafbebfdb032ab68ab2195d8cde12cb2be74
Nilton Pedro da S. Junior <nilton_silva@virgo.atlantico.net.br>
2020-10-27 20:25:33 -0300

    Metodo post conectado com isomnia

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 
-
 const app = express();
 
 app.use(express.json());
@@ -9,3 +8,10 @@ app.listen(3333, () => {
   console.log("Servidor rodando com sucesso na porta 3333");
 });
 
+app.post('/soma', (request, response) => {
+  const { a, b } = request.body;
+
+  const soma = `A soma dos dois números inseridos: ${a + b}`;
+
+  return response.json(soma)
+})
